cc8a8b8c811a0b8323c827f6734c13c49c40bc85
Margen de Error <margendeerror.mde@gmail.com>
2026-06-23 14:02:59 -0300

    chore: purgar tinacms de las dependencias y scripts para optimizar y estabilizar el despliegue en vercel

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'dummy-token', queries,  });
-export default client;
-  
